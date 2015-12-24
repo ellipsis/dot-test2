694b59bac57eb8a0335489921074801bc83391f4
zeekay <zk@monoid.io>
2015-12-24 10:44:37 -0600

    Remove unnecessary files.

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,24 +1,8 @@
 #!/usr/bin/env bash
 #
-# zeekay/test
+# zeekay/test2
 #
 # Test package.
 
 pkg.link() {
-    fs.link_files common
-
-    case $(os.platform) in
-        cygwin)
-            fs.link_files platform/cygwin
-            ;;
-        osx)
-            fs.link_files platform/osx
-            ;;
-        freebsd)
-            fs.link_files platform/freebsd
-            ;;
-        linux)
-            fs.link_files platform/linux
-            ;;
-    esac
 }
